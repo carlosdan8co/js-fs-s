3586d655111ff56ef5193acdc7d73172caf9b4a2
carlosdan8co <yosoy@carlosdan8.co>
2020-11-05 16:32:39 -0600

    11 JavaScript service clases, fetch, y AJAX

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
 const path = require('path');
-
+const cors = require('cors');
 
 //Initializations
 const app= express();
@@ -30,6 +30,7 @@ const storage=multer.diskStorage({
 app.use(multer({storage}).single('image'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cors()); 
 
 //Routes
 app.use('/api/books',require('./routes/books'));
